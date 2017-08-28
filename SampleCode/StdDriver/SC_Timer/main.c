b74b662519d0eb2c67b404d37889961ccfc2266d
Yi-An Chen <yachen@nuvoton.com>
2017-08-28 15:02:47 +0800

    Update timer and smartcard sample codes
    1. Add SC_Timer
    2. Add TIMER_CapaureCounter
    3. Rename Timer_* to TIMER_*
    
    Change-Id: I37c6b74f8f50eebe22deed6c9475fc4aea60d15b

---
+++ SampleCode/StdDriver/SC_Timer/main.c
@@ -0,0 +1,93 @@
+/******************************************************************************
+ * @file     main.c
+ * @version  V1.00
+ * @brief    Demonstrate how to use SC embedded timer
+ *
+  * @copyright (C) 2017 Nuvoton Technology Corp. All rights reserved.
+*****************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+
+
+void SC0_IRQHandler(void)
+{
+    static uint32_t sec = 1;
+
+    if (SC0->INTSTS & SC_INTSTS_TMR0IF_Msk) {
+        /* Clear interrupt flag */
+        SC0->INTSTS = SC_INTSTS_TMR0IF_Msk;
+        printf("%d sec\n", sec++);
+    }
+
+    return;
+}
+
+void SYS_Init(void)
+{
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable External XTAL (4~24 MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Waiting for 12MHz clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK_SetCoreClock(FREQ_192MHZ);
+
+    /* Set both PCLK0 and PCLK1 as HCLK/2 */
+    CLK->PCLKDIV = CLK_PCLKDIV_PCLK0DIV2 | CLK_PCLKDIV_PCLK1DIV2;
+
+    /* Enable IP clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_EnableModuleClock(SC0_MODULE);
+
+    /* Select IP clock source */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HXT, CLK_CLKDIV0_UART0(1));
+    CLK_SetModuleClock(SC0_MODULE, CLK_CLKSEL3_SC0SEL_HXT, CLK_CLKDIV1_SC0(1));
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
+    SystemCoreClockUpdate();
+
+    /* Set PD multi-function pins for UART0 RXD and TXD */
+    SYS->GPD_MFPL = SYS_GPD_MFPL_PD2MFP_UART0_RXD | SYS_GPD_MFPL_PD3MFP_UART0_TXD;
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+
+int main(void)
+{
+    /* Init System, IP clock and multi-function I/O
+       In the end of SYS_Init() will issue SYS_LockReg()
+       to lock protected register. If user want to write
+       protected register, please issue SYS_UnlockReg()
+       to unlock protected register if necessary */
+    SYS_Init();
+
+    /* Init UART to 115200-8n1 for print message */
+    UART_Open(UART0, 115200);
+
+    printf("\nThis sample code demo how to use SC embedded timer. \n");
+
+    /* Open smartcard interface */
+    SC_Open(SC0, SC_PIN_STATE_IGNORE, SC_PIN_STATE_HIGH);
+
+    /* Enable SC TIMER0 interrupt */
+    SC_ENABLE_INT(SC0, SC_INTEN_TMR0IEN_Msk);
+    NVIC_EnableIRQ(SC0_IRQn);
+
+    /* Real ETU divider value is "11+1=12", and the duration of each ETU is 12/SC_CLK */
+    SC0->ETUCTL = 11;
+
+    /* each 1,000,000 ETU will generate TIMER0 timeout event */
+    SC_StartTimer(SC0, 0, SC_TMR_MODE_4, 1000000);  // timer counter will be reloaded.
+
+    while (1);
+}
+
+/*** (C) COPYRIGHT 2017 Nuvoton Technology Corp. ***/
---
+++ SampleCode/StdDriver/TIMER_CaptureCounter/main.c
@@ -0,0 +1,245 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @version  V1.00
+ * @brief    Show how to use the timer2 capture function to capture timer2 counter value.
+ *
+ *
+ * @copyright (C) 2017 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+
+volatile uint32_t g_au32TMRINTCount[4] = {0};
+
+void TMR2_IRQHandler(void)
+{
+    if(TIMER_GetCaptureIntFlag(TIMER2) == 1)
+    {
+        /* Clear Timer2 capture trigger interrupt flag */
+        TIMER_ClearCaptureIntFlag(TIMER2);
+
+        g_au32TMRINTCount[2]++;
+    }
+}
+
+void SYS_Init(void)
+{
+    /* Enable External XTAL (4~24 MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Waiting for 12MHz clock ready */
+    CLK_WaitClockReady( CLK_STATUS_HXTSTB_Msk);
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK_SetCoreClock(FREQ_192MHZ);
+
+    /* Set both PCLK0 and PCLK1 as HCLK/2 */
+    CLK->PCLKDIV = CLK_PCLKDIV_PCLK0DIV2 | CLK_PCLKDIV_PCLK1DIV2;
+
+    /* Enable UART peripheral clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+
+    /* Enable TIMER peripheral clock */
+    CLK_EnableModuleClock(TMR0_MODULE);
+    CLK_EnableModuleClock(TMR2_MODULE);
+    CLK_EnableModuleClock(TMR3_MODULE);
+    CLK_SetModuleClock(TMR0_MODULE, CLK_CLKSEL1_TMR0SEL_HXT, 0);
+    CLK_SetModuleClock(TMR2_MODULE, CLK_CLKSEL1_TMR2SEL_HIRC, 0);
+    CLK_SetModuleClock(TMR3_MODULE, CLK_CLKSEL1_TMR3SEL_HXT, 0);
+
+    /* Set GPD multi-function pins for UART0 RXD and TXD */
+    SYS->GPD_MFPL = SYS_GPD_MFPL_PD2MFP_UART0_RXD | SYS_GPD_MFPL_PD3MFP_UART0_TXD;
+    
+    /* Set multi-function pins for Timer0/Timer3 toggle-output pin and Timer2 event counter pin */
+    SYS->GPG_MFPL = SYS_GPG_MFPL_PG2MFP_TM0 | SYS_GPG_MFPL_PG4MFP_TM2;
+    SYS->GPF_MFPH = SYS_GPF_MFPH_PF11MFP_TM3;
+
+    /* Set multi-function pin for Timer2 external capture pin */
+    SYS->GPH_MFPL = SYS_GPH_MFPL_PH2MFP_TM2_EXT;
+}
+
+
+int main(void)
+{
+    uint32_t u32InitCount;
+    uint32_t au32CAPValue[10], u32CAPDiff;
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Init UART for printf */
+    UART_Open(UART0, 115200);
+
+
+    printf("\n\nCPU @ %d Hz\n", SystemCoreClock);
+    printf("+------------------------------------------+\n");
+    printf("|    Timer2 Capture Counter Sample Code    |\n");
+    printf("+------------------------------------------+\n\n");
+
+    printf("# Timer0 Settings:\n");
+    printf("    - Clock source is HXT\n");
+    printf("    - Time-out frequency is 1000 Hz\n");
+    printf("    - Toggle-output mode and frequency is 500 Hz\n");
+    printf("# Timer3 Settings:\n");
+    printf("    - Clock source is HXT\n");
+    printf("    - Time-out frequency is 2 Hz\n");
+    printf("    - Toggle-output mode and frequency is 1 Hz\n");
+    printf("# Timer2 Settings:\n");
+    printf("    - Clock source is HIRC              \n");
+    printf("    - Continuous counting mode          \n");
+    printf("    - Interrupt enable                  \n");
+    printf("    - Compared value is 0xFFFFFF        \n");
+    printf("    - Event counter mode enable         \n");
+    printf("    - External capture mode enable      \n");
+    printf("    - Capture trigger interrupt enable  \n");
+    printf("# Connect TM0(PG.2) toggle-output pin to TM2(PG.4) event counter pin.\n");
+    printf("# Connect TM3(PF.11) toggle-output pin to TM2_EXT(PH.2) external capture pin.\n\n");
+
+    /* Enable Timer2 NVIC */
+    NVIC_EnableIRQ(TMR2_IRQn);
+
+    /* Open Timer0 in toggle-output mode and toggle-output frequency is 500 Hz*/
+    TIMER_Open(TIMER0, TIMER_TOGGLE_MODE, 1000);
+
+    /* Open Timer3 in toggle-output mode and toggle-output frequency is 1 Hz */
+    TIMER_Open(TIMER3, TIMER_TOGGLE_MODE, 2);
+
+    /* Enable Timer2 event counter input and external capture function */
+    TIMER_Open(TIMER2, TIMER_CONTINUOUS_MODE, 1);
+    TIMER_SET_PRESCALE_VALUE(TIMER2, 0);
+    TIMER_SET_CMP_VALUE(TIMER2, 0xFFFFFF);
+    TIMER_EnableEventCounter(TIMER2, TIMER_COUNTER_EVENT_FALLING);
+    TIMER_EnableCapture(TIMER2, TIMER_CAPTURE_FREE_COUNTING_MODE, TIMER_CAPTURE_EVENT_FALLING);
+    TIMER_EnableInt(TIMER2);
+    TIMER_EnableCaptureInt(TIMER2);
+
+    /* case 1. */
+    printf("# Period between two falling edge captured event should be 500 counts.\n");
+
+    /* Clear Timer2 interrupt counts to 0 */
+    u32InitCount = g_au32TMRINTCount[2] = 0;
+
+    /* Start Timer0, Timer3 and Timer2 counting */
+    TIMER_Start(TIMER0);
+    TIMER_Start(TIMER3);
+    TIMER_Start(TIMER2);
+
+    /* Check Timer2 capture trigger interrupt counts */
+    while(g_au32TMRINTCount[2] <= 10)
+    {
+        if(g_au32TMRINTCount[2] != u32InitCount)
+        {
+            au32CAPValue[u32InitCount] = TIMER_GetCaptureData(TIMER2);
+            if(u32InitCount ==  0)
+            {
+                printf("    [%2d]: %4d. (1st captured value)\n", g_au32TMRINTCount[2], au32CAPValue[u32InitCount]);
+                if(au32CAPValue[u32InitCount] != 0)   // First capture event will reset counter value
+                {
+                    printf("*** FAIL ***\n");
+                    while(1);
+                }
+            }
+            else if(u32InitCount ==  1)
+            {
+                printf("    [%2d]: %4d. (2nd captured value) \n", g_au32TMRINTCount[2], au32CAPValue[u32InitCount]);
+                if(au32CAPValue[u32InitCount] != 500)   // Second event gets two capture event duration counts directly
+                {
+                    printf("*** FAIL ***\n");
+                    while(1);
+                }
+            }
+            else
+            {
+                u32CAPDiff = au32CAPValue[u32InitCount] - au32CAPValue[u32InitCount - 1];
+                printf("    [%2d]: %4d. Diff: %d.\n", g_au32TMRINTCount[2], au32CAPValue[u32InitCount], u32CAPDiff);
+                if(u32CAPDiff != 500)
+                {
+                    printf("*** FAIL ***\n");
+                    while(1);
+                }
+            }
+            u32InitCount = g_au32TMRINTCount[2];
+        }
+    }
+    printf("*** PASS ***\n\n");
+
+    /* case 2. */
+    TIMER_StopCapture(TIMER2);
+    TIMER_Stop(TIMER2);
+    while(TIMER_IS_ACTIVE(TIMER2));
+    TIMER_ClearIntFlag(TIMER2);
+    TIMER_ClearCaptureIntFlag(TIMER2);
+    /* Enable Timer2 event counter input and external capture function */
+    TIMER_Open(TIMER2, TIMER_CONTINUOUS_MODE, 1);
+    TIMER_SET_PRESCALE_VALUE(TIMER2, 0);
+    TIMER_SET_CMP_VALUE(TIMER2, 0xFFFFFF);
+    TIMER_EnableEventCounter(TIMER2, TIMER_COUNTER_EVENT_FALLING);
+    TIMER_EnableCapture(TIMER2, TIMER_CAPTURE_FREE_COUNTING_MODE, TIMER_CAPTURE_EVENT_GET_LOW_PERIOD);
+    TIMER_EnableInt(TIMER2);
+    TIMER_EnableCaptureInt(TIMER2);
+    TIMER_Start(TIMER2);
+
+    printf("# Get first low duration should be 250 counts.\n");
+    printf("# And follows duration between two rising edge captured event should be 500 counts.\n");
+
+    /* Clear Timer2 interrupt counts to 0 */
+    u32InitCount = g_au32TMRINTCount[2] = 0;
+
+    /* Enable Timer2 event counter input and external capture function */
+    TIMER2->CMP = 0xFFFFFF;
+    TIMER2->CTL = TIMER_CTL_CNTEN_Msk | TIMER_CTL_INTEN_Msk | TIMER_CTL_EXTCNTEN_Msk | TIMER_CONTINUOUS_MODE;
+    TIMER2->EXTCTL = TIMER_EXTCTL_CAPEN_Msk | TIMER_CAPTURE_FREE_COUNTING_MODE | TIMER_CAPTURE_EVENT_GET_LOW_PERIOD | TIMER_EXTCTL_CAPIEN_Msk;
+
+    /* Check Timer2 capture trigger interrupt counts */
+    while(g_au32TMRINTCount[2] <= 10)
+    {
+        if(g_au32TMRINTCount[2] != u32InitCount)
+        {
+            au32CAPValue[u32InitCount] = TIMER_GetCaptureData(TIMER2);
+            if(u32InitCount ==  0)
+            {
+                printf("    [%2d]: %4d. (1st captured value)\n", g_au32TMRINTCount[2], au32CAPValue[u32InitCount]);
+                if(au32CAPValue[u32InitCount] != 0)   // First capture event will reset counter value
+                {
+                    printf("*** FAIL ***\n");
+                    while(1);
+                }
+            }
+            else if(u32InitCount ==  1)
+            {
+                printf("    [%2d]: %4d. (2nd captured value)\n", g_au32TMRINTCount[2], au32CAPValue[u32InitCount]);
+                if(au32CAPValue[u32InitCount] != 250)   // Get low duration counts directly
+                {
+                    printf("*** FAIL ***\n");
+                    while(1);
+                }
+            }
+            else
+            {
+                u32CAPDiff = au32CAPValue[u32InitCount] - au32CAPValue[u32InitCount - 1];
+                printf("    [%2d]: %4d. Diff: %d.\n", g_au32TMRINTCount[2], au32CAPValue[u32InitCount], u32CAPDiff);
+                if(u32CAPDiff != 500)
+                {
+                    printf("*** FAIL ***\n");
+                    while(1);
+                }
+            }
+            u32InitCount = g_au32TMRINTCount[2];
+        }
+    }
+
+    /* Stop Timer0, Timer2 and Timer3 counting */
+    TIMER_Stop(TIMER0);
+    TIMER_Stop(TIMER2);
+    TIMER_Stop(TIMER3);
+
+    printf("*** PASS ***\n");
+
+    while(1)
+    {
+        ;
+    };
+}
+
+/*** (C) COPYRIGHT 2017 Nuvoton Technology Corp. ***/
