13085419bcc5344b0d78346b1e0f6d9eb3faf977
mjchen <mjchen@nuvoton.com>
2018-09-14 13:52:28 +0800

    update semihost sample for support MDK5.xx and GCC
    Change-Id: I4d3894d83c848b7c4ae388120ed5e8ced93b1072

--- SampleCode/Semihost/main.c
+++ SampleCode/Semihost/main.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include "NuMicro.h"
 
-# if defined (__GNUC__)
+#if defined (__GNUC__) && !defined(__ARMCC_VERSION) && defined(OS_USE_SEMIHOSTING)
 extern void initialise_monitor_handles(void);
 #endif
 
@@ -61,7 +61,7 @@ int32_t main()
     /* Init System, IP clock and multi-function I/O */
     SYS_Init();
 
-# if defined (__GNUC__)
+#if defined (__GNUC__) && !defined(__ARMCC_VERSION) && defined(OS_USE_SEMIHOSTING)
     initialise_monitor_handles();
 #endif
 
