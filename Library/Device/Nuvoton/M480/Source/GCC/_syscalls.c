293b00cc842d33df1cef0000767d60f816c50155
mjchen <mjchen@nuvoton.com>
2018-10-16 14:36:07 +0800

    update _syscall.c for new GNU tool-chain
    Change-Id: I138ea308c11eb44f4c53c216fff8d9c7fc3118a8

--- Library/Device/Nuvoton/M480/Source/GCC/_syscalls.c
+++ Library/Device/Nuvoton/M480/Source/GCC/_syscalls.c
@@ -537,7 +537,7 @@ static int newslot (void);
 register char* stack_ptr asm ("sp");
 
 /* following is copied from libc/stdio/local.h to check std streams */
-extern void _EXFUN(__sinit,(struct _reent*));
+extern void __sinit(struct _reent*);
 #define CHECK_INIT(ptr) \
     do                                          \
     {                                           \
