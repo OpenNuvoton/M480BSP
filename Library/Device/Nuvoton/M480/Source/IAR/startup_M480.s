OpenNuvoton/M480BSP

commit b40933fbf8ce3551306489a633102993d26ba4e7

Add CCAP_IRQHandler to GCC and IAR
Change-Id: I73eceff0e6e6ad5b436be79a3c6d186f3b1a129e

Library/Device/Nuvoton/M480/Source/IAR/startup_M480.s
@@ -126,7 +126,7 @@ __vector_table_0x1c
         DCD     BPWM0_IRQHandler          ; 78: BPWM0
         DCD     BPWM1_IRQHandler          ; 79: BPWM1
         DCD     SPIM_IRQHandler           ; 80: SPIM
-        DCD     Default_Handler           ; 81: ToDo: Add description to this Interrupt
+        DCD     CCAP_IRQHandler           ; 81: CCAP
         DCD     I2C2_IRQHandler           ; 82: I2C2
         DCD     Default_Handler           ; 83:
         DCD     QEI0_IRQHandler           ; 84: QEI0
@@ -325,6 +325,7 @@ SysTick_Handler
         PUBWEAK  BPWM0_IRQHandler
         PUBWEAK  BPWM1_IRQHandler
         PUBWEAK  SPIM_IRQHandler
+        PUBWEAK  CCAP_IRQHandler
         PUBWEAK  I2C2_IRQHandler
         PUBWEAK  QEI0_IRQHandler
         PUBWEAK  QEI1_IRQHandler

Library/Device/Nuvoton/M480/Source/GCC/startup_M480.S
@@ -146,7 +146,7 @@ __Vectors:
 	.long	BPWM0_IRQHandler      /* 78: BPWM0                      */
 	.long	BPWM1_IRQHandler      /* 79: BPWM1                      */
 	.long	SPIM_IRQHandler       /* 80: SPIM                       */
-	.long	0                     /* 81: Reserved                   */
+	.long	CCAP_IRQHandler       /* 81: CCAP                       */
 	.long	I2C2_IRQHandler       /* 82: I2C2                       */
 	.long	0                     /* 83: Reserved                   */
 	.long	QEI0_IRQHandler       /* 84: QEI0                       */
@@ -451,6 +451,7 @@ Default_Handler:
 	def_irq_handler	BPWM0_IRQHandler
 	def_irq_handler	BPWM1_IRQHandler
 	def_irq_handler	SPIM_IRQHandler
+	def_irq_handler CCAP_IRQHandler
 	def_irq_handler	I2C2_IRQHandler
 	def_irq_handler	QEI0_IRQHandler
 	def_irq_handler	QEI1_IRQHandler

commit 76a62ac6fe49c95d1566c1e5fb0489b0665a3895

Fix IAR project build failed issue caused by b40933fbf
Change-Id: I8ad500af61120d402570bad0a3f28a0abfad2e32

Library/Device/Nuvoton/M480/Source/IAR/startup_M480.s
@@ -422,6 +422,7 @@ USCI1_IRQHandler
 BPWM0_IRQHandler
 BPWM1_IRQHandler
 SPIM_IRQHandler
+CCAP_IRQHandler
 I2C2_IRQHandler
 QEI0_IRQHandler
 QEI1_IRQHandler