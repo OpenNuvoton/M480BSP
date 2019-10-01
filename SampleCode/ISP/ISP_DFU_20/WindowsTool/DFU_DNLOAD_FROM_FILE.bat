a03dc6056ad3b8a3037e0edc4c8c015f1fb27a35
CFLi0 <cfli0@nuvoton.com>
2019-10-01 09:58:07 +0800

    Update Windows driver.
    Change-Id: I6f4d6987a8ed9082786d2e2e1176a21e7bfda43b

---
+++ SampleCode/ISP/ISP_DFU_20/WindowsTool/DFU_DNLOAD_FROM_FILE.bat
@@ -0,0 +1,3 @@
+
+.\dfu-util.exe -a 0 -d 0x416:0xBDF0 -D .\DFU_DNLOAD_PATTERN.bin
+PAUSE
---
+++ SampleCode/ISP/ISP_DFU_20/WindowsTool/DFU_UPLOAD_TO_FILE.bat
@@ -0,0 +1,3 @@
+Del .\DFU_UPLOAD_DAT.bin
+.\dfu-util.exe -a 0 -d 0x416:0xBDF0  -U .\DFU_UPLOAD_DAT.bin
+PAUSE
