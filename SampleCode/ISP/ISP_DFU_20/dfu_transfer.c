498295b60a4d7e1940848b4f83127791446d730a
MS10 HPChen0 <HPChen0@nuvoton.com>
2019-12-30 10:28:56 +0800

    [HSUSBD] Fixed DFU upload issue
    Change-Id: I36a04c191c9316b1f7c299346bc0cb63732821dd

--- SampleCode/ISP/ISP_DFU_20/dfu_transfer.c
+++ SampleCode/ISP/ISP_DFU_20/dfu_transfer.c
@@ -257,6 +257,7 @@ void DFU_ClassRequest(void)
                         {
                             dfu_status.bState = STATE_dfuIDLE;
                             HSUSBD->CEPCTL |= HSUSBD_CEPCTL_ZEROLEN_Msk;
+                            while (HSUSBD->CEPCTL & HSUSBD_CEPCTL_ZEROLEN_Msk);
                             /* Status stage */
                             HSUSBD_CLR_CEP_INT_FLAG(HSUSBD_CEPINTSTS_STSDONEIF_Msk);
                             HSUSBD_SET_CEP_STATE(HSUSBD_CEPCTL_NAKCLR);
