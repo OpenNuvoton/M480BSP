f208e86cfc6f0ec64369dde3931f13038669c26e
schung <schung@nuvoton.com>
2020-12-04 09:19:57 +0800

    Fix PDMA comment
    Change-Id: Ic7b733074206dd8170a12aa4a6ed68665f4ae823

--- Library/Device/Nuvoton/M480/Include/pdma_reg.h
+++ Library/Device/Nuvoton/M480/Include/pdma_reg.h
@@ -81,7 +81,7 @@ typedef struct
      * |        |          |0 = Stride transfer mode Disabled.
      * |        |          |1 = Stride transfer mode Enabled.
      * |[31:16] |TXCNT     |Transfer Count
-     * |        |          |The TXCNT represents the required number of PDMA transfer, the real transfer count is (TXCNT + 1); The maximum transfer count is 16384 , every transfer may be byte, half-word or word that is dependent on TXWIDTH field.
+     * |        |          |The TXCNT represents the required number of PDMA transfer, the real transfer count is (TXCNT + 1); The maximum transfer count is 32768, every transfer may be byte, half-word or word that is dependent on TXWIDTH field.
      * |        |          |Note: When PDMA finish each transfer data, this field will be decrease immediately.
      * @var DSCT_T::SA
      * Offset: 0x04  Source Address Register of PDMA Channel n
