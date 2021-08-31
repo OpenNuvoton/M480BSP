9db78044acdb151b14262c6fe1d4e64bc48e9fbb
Yi-An Chen <yachen@nuvoton.com>
2021-08-31 10:15:23 +0800

    Revert "Add g_i32ErrCode declaration in NuMicro.h"
    This reverts commit d6cc4715a34992e7be40cf470011c5aad87d8daf.

--- Library/Device/Nuvoton/M480/Include/NuMicro.h
+++ Library/Device/Nuvoton/M480/Include/NuMicro.h
@@ -1,17 +1,15 @@
 /**************************************************************************//**
  * @file     NuMicro.h
- * @version  V1.01
+ * @version  V1.00
  * @brief    NuMicro peripheral access layer header file.
  *
  * SPDX-License-Identifier: Apache-2.0
- * @copyright (C) 2017-2021 Nuvoton Technology Corp. All rights reserved.
+ * @copyright (C) 2017-2020 Nuvoton Technology Corp. All rights reserved.
  *****************************************************************************/
 #ifndef __NUMICRO_H__
 #define __NUMICRO_H__
 
 #include "M480.h"
-
-int32_t g_i32ErrCode = 0;
 
 #endif  /* __NUMICRO_H__ */
 
